f5474d3e8a2697f2875f66713b67bd2969bbcb2f
Thiago Retamiro <thiago.retamiro@hotmail.com>
2025-10-29 21:04:58 -0300

    Finalizando o desafio SPORE

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,11 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var confString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(confString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
